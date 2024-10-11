8a25906fe1d7a843fe0bee83f26ed3977804c6e8
eungobs <eungobs@gmail.com>
2024-10-11 11:21:09 +0200

    index vercel files

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, Heroku!');
+    res.send('server!');
 });
 
 app.listen(PORT, () => {
